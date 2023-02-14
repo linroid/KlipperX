3756c810548ed4cfd7530a59ce08ebf5f62ca602
Lin Zhang <linroid@gmail.com>
2023-02-15 00:21:26 +0800

    Reduce duplicate gradle configurations

--- buildSrc/src/main/kotlin/multiplatform-compose-setup.gradle.kts
+++ buildSrc/src/main/kotlin/multiplatform-compose-setup.gradle.kts
@@ -1,31 +1,16 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-multiplatform")
+    id("multiplatform-setup")
     id("org.jetbrains.compose")
 }
 
 kotlin {
-    android()
-    jvm("desktop") {
-        jvmToolchain(11)
-    }
-    ios()
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
     sourceSets {
-        val commonMain by getting {
+        getByName("commonMain") {
             dependencies {
                 api(compose.runtime)
                 api(compose.foundation)
                 api(compose.material)
                 api(compose.ui)
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
             }
         }
 
@@ -35,13 +20,11 @@ kotlin {
             }
         }
 
-        val iosMain by getting { dependsOn(commonMain) }
-        val iosTest by getting { dependsOn(commonTest) }
-        getByName("iosX64Main") { dependsOn(iosMain) }
-        getByName("iosX64Test") { dependsOn(iosMain) }
-        getByName("iosArm64Main") { dependsOn(iosMain) }
-        getByName("iosArm64Test") { dependsOn(iosMain) }
-        getByName("iosSimulatorArm64Main") { dependsOn(iosMain) }
-        getByName("iosSimulatorArm64Test") { dependsOn(iosTest) }
+        getByName("androidMain") {
+            dependencies {
+                api("androidx.appcompat:appcompat:1.6.1")
+                api("androidx.core:core-ktx:1.9.0")
+            }
+        }
     }
 }
--- buildSrc/src/main/kotlin/multiplatform-setup.gradle.kts
+++ buildSrc/src/main/kotlin/multiplatform-setup.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library")
+    id("android-setup")
     id("kotlin-multiplatform")
 }
 
