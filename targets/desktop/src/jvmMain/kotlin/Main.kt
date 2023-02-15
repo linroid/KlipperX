c1459ad53b314b829ac5510685bb592b23c3b685
Lin Zhang <linroid@gmail.com>
2023-02-15 22:39:14 +0800

    Add link for more client properties

--- targets/desktop/src/jvmMain/kotlin/Main.kt
+++ targets/desktop/src/jvmMain/kotlin/Main.kt
@@ -1,18 +1,26 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import java.awt.Frame
 
 fun main() = application {
     Window(
-        undecorated = true,
+        state = WindowState(width = 800.dp, height = 600.dp),
+        undecorated = false,
         onCloseRequest = ::exitApplication,
         title = "KlipperX"
     ) {
-        window.extendedState = Frame.MAXIMIZED_BOTH
         window.rootPane.apply {
+            // See the link below to get all properties
+            // https://github.com/JetBrains/JetBrainsRuntime/blob/main/src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java#L145
             putClientProperty("apple.awt.fullWindowContent", true)
-            putClientProperty("apple.awt.transparentTitleBar", true)
+            // putClientProperty("apple.awt.transparentTitleBar", false)
+            // putClientProperty("apple.awt.windowAppearance", "NSAppearanceNameVibrantDark")
+            // putClientProperty("apple.awt.windowAppearance", "NSAppearanceNameVibrantLight")
         }
-        MainView()
+        Column {
+            MainView()
+        }
     }
 }
