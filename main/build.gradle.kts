d894091519703f12a1ad863f12f9b8d0aae936b8
Lin Zhang <linroid@gmail.com>
2023-02-17 22:06:29 +0800

    Add unit test workflow for iOS

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     cocoapods {
         summary = "KotlinX Compose Multiplatform Shared Module"
         homepage = "https://github.com/linroid/KlipperX"
-        ios.deploymentTarget = "14.1"
+        ios.deploymentTarget = "15.2"
         podfile = project.file("../targets/ios/Podfile")
         framework {
             baseName = "main"
